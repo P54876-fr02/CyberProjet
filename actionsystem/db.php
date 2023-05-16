6dee3180e1d6a38617764f77effb8c947b2914ee
P54876-fr02 <meryem.benamar2002@gmail.com>
2023-05-16 12:40:11 +0200

    push

--- actionsystem/db.php
+++ actionsystem/db.php
@@ -3,8 +3,13 @@
         
         $bdd = new PDO('mysql:host=localhost;dbname=login_db;charset=utf8;', 'root', '');
         // print("ok");
+        // Établit une connexion à la base de données MySQL en utilisant PDO.
+       
     } catch (Exception $e) {
+        // Si une exception se produit lors de la tentative de connexion à la base de données, l'erreur est capturée.
        
         die('error : '.$e->getMessage());
+        // Affiche un message d'erreur et arrête l'exécution du script en affichant le message de l'exception.
+
     }
 ?>
