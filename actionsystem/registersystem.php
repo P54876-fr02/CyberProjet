99bf37270be2b59b22238fc677f6732568c26807
P54876-fr02 <meryem.benamar2002@gmail.com>
2023-05-16 12:43:53 +0200

    push

--- actionsystem/registersystem.php
+++ actionsystem/registersystem.php
@@ -3,26 +3,37 @@
     require("db.php");
 
     if(isset($_POST['submit'])){
+        // Vérifie si le formulaire de soumission a été envoyé
+
         
         if (!empty($_POST['username']) AND !empty($_POST['email']) AND !empty($_POST['password']) AND !empty($_POST['repassword']) AND $_POST['password'] == $_POST['repassword']) {
+            // Vérifie que tous les champs requis sont remplis et que les mots de passe correspondent
 
             $username = $_POST['username'];
             $email = $_POST['email'];
             $password = $_POST['password'];
             $repassword = $_POST['repassword'];
 
+            // Prépare les requêtes pour vérifier si le nom d'utilisateur ou l'e-mail existent déjà dans la base de données
             $checkuserifexist = $bdd->prepare("SELECT * FROM admin WHERE username = ? ");
             $checkemailifexist = $bdd->prepare("SELECT * FROM admin WHERE email = ? ");
+
+            // Exécute les requêtes en remplaçant les paramètres par les valeurs réelles
             $checkuserifexist->execute(array($username));
             $checkuserifexist->execute(array($email));
 
             if ($checkemailifexist->rowCount() == 0 && $checkuserifexist->rowCount() == 0) {
+                // Vérifie que ni le nom d'utilisateur ni l'e-mail ne sont déjà présents dans la base de données
+
                 $pwd = password_hash($password, PASSWORD_BCRYPT);
                 $repwd = password_hash($repassword, PASSWORD_BCRYPT);
+                // Hash les mots de passe avant de les stocker dans la base de données
 
                 $insertuser = $bdd->prepare('INSERT INTO admin(username, email, passwords, repassword) VALUES (?, ?, ?, ?)');
-                $insertuser->execute(array($username,$email,$pwd,$repwd));
+                // Prépare la requête d'insertion pour ajouter les données dans la table 'admin'
 
+                $insertuser->execute(array($username,$email,$pwd,$repwd));
+                // Exécute la requête d'insertion en remplaçant les paramètres par les valeurs réelles
                 
 
                 header('Location: ./index.php');
